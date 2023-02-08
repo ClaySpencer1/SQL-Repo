ce3f2929bc2f64a9003e416aecdbb6b3aabd3df3
Clay Spencer <clayspencer52@gmail.com>
2023-02-08 09:44:11 -0600

    done

--- SQL Exercise 2.sql
+++ SQL Exercise 2.sql
@@ -19,7 +19,7 @@ INNER JOIN sales AS s
 ON e.EmployeeID = s.EmployeeID
 GROUP BY e.EmployeeID
 ORDER BY Total DESC
-LIMIT 1;
+LIMIT 5;
 /* joins: find the name of the department, and the name of the category for Appliances and Games */
 SELECT d.Name AS department, c.Name AS Category FROM categories AS c
 INNER JOIN departments AS d
@@ -29,8 +29,8 @@ WHERE c.Name = "Appliances" OR c.Name = "Games";
 
  for Eagles: Hotel California --You may need to use SUM() */
 SELECT 
-p.Name AS product,
-SUM(s.Quantity) AS TotalQuantity,
+p.Name AS Product,
+SUM(s.Quantity) AS "Total Quantity",
 SUM(s.PricePerUnit * s.Quantity) AS TotalValue
 FROM products AS p
 INNER JOIN sales AS s 
@@ -45,6 +45,7 @@ SELECT p.Name as Product, r.Reviewer, r.Rating, r.Comment
 FROM reviews AS r
 INNER JOIN Products as p
 ON p.ProductID = r.ProductID
+WHERE p.Name = 'Visio TV'
 limit 1;
 
 -- ------------------------------------------ Extra - May be difficult
@@ -52,3 +53,9 @@ limit 1;
 /* Your goal is to write a query that serves as an employee sales report.
 
 This query should return the employeeID, the employee's first and last name, the name of each product, how many of that product they sold */
+SELECT employees.EmployeeID, employees.FirstName, employees.LastName, products.Name, sales.Quantity
+FROM employees
+INNER JOIN sales
+ON employees.EmployeeID = sales.EmployeeID
+INNER JOIN products
+ON products.ProductID = sales.ProductID;
